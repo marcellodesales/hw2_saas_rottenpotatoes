3d571bcd18b269741b008030ec7413c7cff8d70e
Marcello de Sales <marcello.desales@gmail.com>
2012-06-18 20:48:02 +0000

    This revision updates the spec to the end of the presentation of the slides of section 5.4 of the SaaS class from Berkeley.
    http://www.scribd.com/doc/86993368/Slides-Chapter5-TDD
    
    - Just adding the specification of another it in the rspec.

--- spec/controllers/our_first_controller_spec.rb
+++ spec/controllers/our_first_controller_spec.rb
@@ -6,5 +6,12 @@ describe MoviesController do
       Movie.should_receive(:find_in_tmdb).with('hardware')
       post :search_tmdb, {:search_terms => 'hardware'}
     end
+
+    it 'should select the Search Results template for rendering' do
+      Movie.stub(:find_in_tmdb) # see the meaning on presentation 5.4
+      post :search_tmdb, {:search_terms => 'hardware'}
+      # response is the response object 
+      response.should render_template('search_tmdb')
+    end
   end
 end
